070ff96525560a45184871f2d4f8686cd2bc4ac9
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-20 18:08:23 +0900

    temp: 카드 화면 임시 저장

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/test-views/main-view-with-velog',
+      name: 'mainViewWithVelog',
+      component: () => import('@/testViews/MainViewWithVelog.vue'),
+    },
+    {
       path: '/test-views/sidebar-layout',
       name: 'sidebarLayout',
       component: () => import('@/testViews/SidebarLayoutViews.vue'),
