6ffd12c83d13cd919d7bf3bec23632de7e248e73
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-22 22:03:57 +0900

    feat: 사이드바2 보일러플레이트 구현

--- src/components/sidebars/BaseSidebar2.vue
+++ src/components/sidebars/BaseSidebar2.vue
@@ -3,49 +3,121 @@
 <template>
   <v-navigation-drawer>
     <v-container class="d-flex align-center">
-      <v-icon icon="$vuetify" size="x-large" class="mr-2" />
+      <v-icon icon="mdi-debian" size="x-large" class="mr-2" />
       <span class="text-h6">49EHyeon42</span>
     </v-container>
 
     <v-list density="compact" class="m-0">
-      <v-container class="d-flex align-center">
-        <span class="mr-2 text-no-wrap text-subtitle-1">Apps & Pages</span>
-        <v-divider />
-      </v-container>
-      <v-list-item prepend-icon="mdi-email" to="/">
-        <span class="text-subtitle-2">EMail </span>
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-home</v-icon>
+          <span class="text-subtitle-2">Home</span>
+        </div>
       </v-list-item>
-      <v-list-item title="Email" prepend-icon="mdi-email" to="/" class="text-caption" />
-      <v-list-item title="Chat" prepend-icon="mdi-chat" to="/" />
-      <v-list-item title="Calendar" prepend-icon="mdi-calendar" to="/" />
-      <v-list-item title="Account Settings" prepend-icon="mdi-cog" to="/" />
-      <v-list-item title="Sign In" prepend-icon="mdi-login" to="/" />
-      <v-list-item title="Register" prepend-icon="mdi-account-plus" to="/" />
-      <v-list-item title="Error" prepend-icon="mdi-alert-circle" to="/" />
+      <v-list-item to="/about">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-account</v-icon>
+          <span class="text-subtitle-2">About</span>
+        </div>
+      </v-list-item>
 
-      <v-container class="d-flex align-center">
-        <span class="mr-2 text-no-wrap text-subtitle-2">User Interface</span>
+      <v-list-group>
+        <template #activator="{ props }">
+          <v-list-item v-bind="props">
+            <div class="d-flex align-center">
+              <v-icon class="mr-2">mdi-folder</v-icon>
+              <span class="text-subtitle-2">Projects</span>
+            </div>
+          </v-list-item>
+        </template>
+
+        <v-list-item to="/typography" style="padding-left: 32px !important">
+          <div class="d-flex align-center">
+            <v-icon class="mr-2">mdi-application</v-icon>
+            <span class="text-subtitle-2">커넥트립</span>
+          </div>
+        </v-list-item>
+        <v-list-item to="/typography" style="padding-left: 32px !important">
+          <div class="d-flex align-center">
+            <v-icon class="mr-2">mdi-application</v-icon>
+            <span class="text-subtitle-2">전화대장군</span>
+          </div>
+        </v-list-item>
+        <v-list-item to="/typography" style="padding-left: 32px !important">
+          <div class="d-flex align-center">
+            <v-icon class="mr-2">mdi-application</v-icon>
+            <span class="text-subtitle-2">로컬 업</span>
+          </div>
+        </v-list-item>
+      </v-list-group>
+
+      <!-- 보일러플레이트 -->
+      <v-container class="d-flex align-center pa-2">
+        <span class="mr-2 text-no-wrap text-subtitle-1">Contact</span>
         <v-divider />
       </v-container>
-      <v-list-item title="Typography" prepend-icon="mdi-format-text-variant" to="/" />
-      <v-list-item title="Icons" prepend-icon="mdi-star-circle" to="/" />
-      <v-list-item title="Cards" prepend-icon="mdi-credit-card-outline" to="/" />
-      <v-list-item title="Tables" prepend-icon="mdi-table" to="/" />
-      <v-list-item title="Alerts" prepend-icon="mdi-alert" to="/" />
-      <v-list-item title="Dialogs" prepend-icon="mdi-message-alert" to="/" />
+      <v-list-item href="mailto:49ehyeon42@email.com" target="_blank">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-email</v-icon>
+          <span class="text-subtitle-2">Email</span>
+        </div>
+      </v-list-item>
+      <v-list-item href="https://github.com/49EHyeon42" target="_blank">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-github</v-icon>
+          <span class="text-subtitle-2">Github</span>
+        </div>
+      </v-list-item>
 
-      <v-container class="d-flex align-center">
-        <span class="mr-2 text-no-wrap text-subtitle-2">User Interface</span>
+      <v-container class="d-flex align-center pa-2">
+        <span class="mr-2 text-no-wrap text-subtitle-2">Sub Title</span>
         <v-divider />
       </v-container>
-      <v-list-item title="Typography" prepend-icon="mdi-format-text-variant" to="/" />
-      <v-list-item title="Icons" prepend-icon="mdi-star-circle" to="/" />
-      <v-list-item title="Cards" prepend-icon="mdi-credit-card-outline" to="/" />
-      <v-list-item title="Tables" prepend-icon="mdi-table" to="/" />
-      <v-list-item title="Alerts" prepend-icon="mdi-alert" to="/" />
-      <v-list-item title="Dialogs" prepend-icon="mdi-message-alert" to="/" />
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-format-text-variant</v-icon>
+          <span class="text-subtitle-2">Typography</span>
+        </div>
+      </v-list-item>
+
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-star-circle</v-icon>
+          <span class="text-subtitle-2">Icons</span>
+        </div>
+      </v-list-item>
+
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-credit-card-outline</v-icon>
+          <span class="text-subtitle-2">Cards</span>
+        </div>
+      </v-list-item>
+
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-table</v-icon>
+          <span class="text-subtitle-2">Tables</span>
+        </div>
+      </v-list-item>
+
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-alert</v-icon>
+          <span class="text-subtitle-2">Alerts</span>
+        </div>
+      </v-list-item>
+
+      <v-list-item to="/">
+        <div class="d-flex align-center">
+          <v-icon class="mr-2">mdi-message-alert</v-icon>
+          <span class="text-subtitle-2">Dialogs</span>
+        </div>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <style scoped></style>
+
+<!-- text-medium-emphasis 로 검정에서 회색 변경 가능 -->
