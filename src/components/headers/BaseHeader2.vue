89c928af54b0954377d41fc468d7c8b4f0a8072e
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-24 18:52:27 +0900

    fix: .display.mdAndDown 간단하게 변경

--- src/components/headers/BaseHeader2.vue
+++ src/components/headers/BaseHeader2.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 import { useToggleTheme } from '@/composables/useToggleTheme';
-import { useDisplay } from 'vuetify';
 
 const emit = defineEmits(['toggle:drawer']);
 
 const router = useRouter();
 const toggleTheme = useToggleTheme();
-const { mdAndDown } = useDisplay();
 </script>
 
 <template>
   <v-app-bar>
-    <v-app-bar-nav-icon v-if="mdAndDown" @click="emit('toggle:drawer')" />
+    <v-app-bar-nav-icon v-if="$vuetify.display.mdAndDown" @click="emit('toggle:drawer')" />
     <v-app-bar-title @click="router.push('/')">49EHyeon42</v-app-bar-title>
     <template v-slot:append>
       <v-btn icon @click="toggleTheme">
--- src/components/headers/EhHeader2.vue
+++ src/components/headers/EhHeader2.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
 import EhBrand from '../common/EhBrand.vue';
 
-import { useDisplay } from 'vuetify';
-
 import { useToggleTheme } from '@/composables/useToggleTheme';
 
-const { mdAndDown } = useDisplay();
 const { isDark, toggleTheme } = useToggleTheme();
 
 const drawer = defineModel<boolean>('drawer');
@@ -13,7 +10,7 @@ const drawer = defineModel<boolean>('drawer');
 
 <template>
   <v-app-bar elevation="0">
-    <template v-if="mdAndDown">
+    <template v-if="$vuetify.display.mdAndDown">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-app-bar-title>
         <EhBrand />
