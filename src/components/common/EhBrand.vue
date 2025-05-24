8013a9a17581999642b3d5371bdf924128183ea3
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-24 19:09:14 +0900

    fix: brand 라우터 추가

--- src/components/common/EhBrand.vue
+++ src/components/common/EhBrand.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
 
 <template>
-  <div class="d-flex align-center brand-gap">
+  <div class="d-flex align-center cursor-pointer brand-gap" @click="router.push('/')">
     <v-icon size="32">mdi-debian</v-icon>
     <span class="text-h6">49EHyeon42</span>
   </div>
