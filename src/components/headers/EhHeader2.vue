a353c81018ca438337f5e31c4d18b608fbddc7b1
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-25 20:15:41 +0900

    fix: 헤더, 유튜브와 동일하게 조절

--- src/components/headers/EhHeader2.vue
+++ src/components/headers/EhHeader2.vue
@@ -9,7 +9,7 @@ const drawer = defineModel<boolean>('drawer');
 </script>
 
 <template>
-  <v-app-bar elevation="0">
+  <v-app-bar density="compact" elevation="0">
     <template v-if="$vuetify.display.mdAndDown">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-app-bar-title>
@@ -18,7 +18,9 @@ const drawer = defineModel<boolean>('drawer');
     </template>
     <template v-slot:append>
       <v-btn icon @click="themeStore.toggleTheme">
-        <v-icon>{{ themeStore.isDark ? 'mdi-weather-night' : 'mdi-white-balance-sunny' }}</v-icon>
+        <v-icon size="x-large">{{
+          themeStore.isDark ? 'mdi-weather-night' : 'mdi-white-balance-sunny'
+        }}</v-icon>
       </v-btn>
     </template>
   </v-app-bar>
