d553aa1f4e1a60159047bb68d198fad4eac11b50
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-25 21:21:23 +0900

    feat: scroll to top 버튼 구현

--- src/layouts/EhLayout2.vue
+++ src/layouts/EhLayout2.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { useDisplay } from 'vuetify';
 
 import Ehsidebar2 from '@/components/sidebars/EhSidebar2.vue';
@@ -9,6 +9,23 @@ import EhFooter2 from '@/components/footers/EhFooter2.vue';
 const { mdAndDown } = useDisplay();
 
 const drawer = ref(!mdAndDown.value);
+const showScrollToTopButton = ref(false);
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+const handleScrollEvent = () => {
+  showScrollToTopButton.value = window.scrollY > 0;
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScrollEvent);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScrollEvent);
+});
 </script>
 
 <template>
@@ -19,6 +36,22 @@ const drawer = ref(!mdAndDown.value);
       <slot />
     </v-main>
     <EhFooter2 />
+
+    <!-- scroll to top button -->
+    <!-- footer 때문에 z-index: 905 사용 -->
+    <v-slide-y-reverse-transition>
+      <v-btn
+        v-if="showScrollToTopButton"
+        color="primary"
+        icon="mdi-chevron-up"
+        location="bottom end"
+        position="fixed"
+        size="x-small"
+        class="ma-3"
+        style="z-index: 905"
+        @click="scrollToTop"
+      ></v-btn>
+    </v-slide-y-reverse-transition>
   </v-layout>
 </template>
 
