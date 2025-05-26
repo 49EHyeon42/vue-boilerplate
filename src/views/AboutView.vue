ded9eda110b9d6d7cdebd343ce718c54accb8e41
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-26 21:58:29 +0900

    temp: About 임시 저장

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,65 @@
 <script setup lang="ts">
 import EhLayout2 from '@/layouts/EhLayout2.vue';
+
+const awards = [
+  {
+    date: '2024.11.21',
+    title: '고용노동부 장관상(최우수상)',
+    description: '2024년 제6회 첨단산업•디지털 핵심 실무인재 양성훈련 해커톤',
+  },
+  {
+    date: '2024.10.11',
+    title: '우수 수료생',
+    description: '카카오 테크 부트캠프 클라우드 네이티브 과정 1기',
+  },
+  {
+    date: '2024.10.11',
+    title: '파이널 프로젝트 최우수 팀',
+    description: '카카오 테크 부트캠프 클라우드 네이티브 과정 1기',
+  },
+];
+
+const certifications = [
+  { title: '정보처리기사', date: '2023.09.01' },
+  {
+    title: 'CSTS(Certified Software Test Specialist, 소프트웨어 테스트 전문가) FL',
+    date: '2023.06.17',
+  },
+  { title: '리눅스마스터 2급', date: '2023.03.31' },
+  { title: 'SQLD(SQL Developer, SQL 개발자)', date: '2022.12.02' },
+];
 </script>
 
 <template>
   <EhLayout2>
     <v-container fluid>
       <v-row>
+        <!-- 수상 카드 -->
+        <v-col cols="12" class="d-flex">
+          <v-card class="w-100">
+            <v-card-title>수상</v-card-title>
+            <v-card-text>
+              <v-table>
+                <thead>
+                  <tr>
+                    <th>날짜</th>
+                    <th>수상/성과</th>
+                    <th>내용</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(award, index) in awards" :key="index">
+                    <td>{{ award.date }}</td>
+                    <td>{{ award.title }}</td>
+                    <td>{{ award.description }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <!-- 교외 활동 카드 -->
         <v-col cols="12" class="d-flex">
           <!-- TODO: 나중에 w-100 빼고 조절 -->
           <v-card class="w-100">
@@ -72,6 +126,29 @@ import EhLayout2 from '@/layouts/EhLayout2.vue';
                   </div>
                 </v-timeline-item>
               </v-timeline>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <!-- 자격증 카드 -->
+        <v-col cols="12" class="d-flex">
+          <v-card class="w-100">
+            <v-card-title>자격증</v-card-title>
+            <v-card-text>
+              <v-table>
+                <thead>
+                  <tr>
+                    <th>취득일</th>
+                    <th>자격증명</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(cert, index) in certifications" :key="index">
+                    <td>{{ cert.date }}</td>
+                    <td>{{ cert.title }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
             </v-card-text>
           </v-card>
         </v-col>
