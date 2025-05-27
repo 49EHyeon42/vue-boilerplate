9734b63af7e1656cdfc83bca4f942489ecce58a4
49EHyeon42 <49ehyeon42@gmail.com>
2025-05-27 21:32:40 +0900

    fix: local storage를 통해 테마를 기억할 수 있도록 수정

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -3,14 +3,35 @@ import { computed } from 'vue';
 import { useTheme } from 'vuetify';
 
 export const useThemeStore = defineStore('theme', () => {
+  const STORAGE_KEY = '49ehyeon42Key';
+
+  const getThemeFromLocalStorage = (): string | null => {
+    try {
+      return JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}').theme ?? null;
+    } catch {
+      return null;
+    }
+  };
+
+  const setThemeInLocalStorage = (theme: string) => {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}') ?? {};
+    data.theme = theme;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  };
+
   const theme = useTheme();
 
-  theme.global.name.value = matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const savedTheme = getThemeFromLocalStorage();
+  const defaultTheme = matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  theme.global.name.value = savedTheme || defaultTheme;
 
   const isDark = computed(() => theme.global.current.value.dark);
 
   const toggleTheme = () => {
     theme.global.name.value = isDark.value ? 'light' : 'dark';
+
+    setThemeInLocalStorage(theme.global.name.value);
   };
 
   return {
