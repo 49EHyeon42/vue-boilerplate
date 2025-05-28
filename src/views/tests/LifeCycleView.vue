8e2ac54574c2268d3e614dfde05e83c3a03e20c7
49ehyeon42 <49ehyeon42@gmail.com>
2025-05-28 09:08:08 +0900

    temp: watch 문 임시 저장

--- src/views/tests/LifeCycleView.vue
+++ src/views/tests/LifeCycleView.vue
@@ -7,14 +7,40 @@ import {
   onUnmounted,
   onUpdated,
   ref,
+  watch,
+  watchEffect,
+  watchPostEffect,
+  watchSyncEffect,
 } from 'vue';
 
 const count = ref(0);
 
+// 1.
+watchSyncEffect(() => {
+  console.log('watchSyncEffect - count:', count.value);
+});
+
+// 2.
+watchEffect(() => {
+  console.log('watchEffect - count:', count.value);
+});
+
+// 3.
+watch(count, (newVal, oldVal) => {
+  console.log('watch - count:', oldVal, '→', newVal);
+});
+
+// 5.
+watchPostEffect(() => {
+  console.log('watchPostEffect - count:', count.value);
+});
+
 onBeforeMount(() => console.log('1. onBeforeMount'));
 onMounted(() => console.log('2. onMounted'));
 
+// 4.
 onBeforeUpdate(() => console.log('3-1. onBeforeUpdate: count 감지'));
+// 6.
 onUpdated(() => console.log('3-2. onUpdated: count 감지'));
 
 onBeforeUnmount(() => console.log('4. onBeforeUnmount'));
